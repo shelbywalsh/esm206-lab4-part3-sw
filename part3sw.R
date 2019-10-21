26fc0dfb4b456e0efac97418abdd0a473fec54b1
shelbywalsh <swalsh@ucsb.edu>
2019-10-21 13:56:37 -0700

    made graph

--- part3sw.R
+++ part3sw.R
@@ -11,3 +11,14 @@ library(here)
 db<- read_csv(here::here("data", "disease_burden.csv")) %>% 
   clean_names() %>% 
   rename(deaths_per_100k = death_rate_per_100_000)
+
+#create sub set and graph
+
+db_sub <- db %>% 
+  filter(country_name %in% c("United States", "Japan", "Afghanistan", "Somalia")) %>% 
+  filter(age_group == "0-6 days", sex == "Both")
+
+#create basic line grapgh
+
+ggplot(data = db_sub, aes(x = year, y = deaths_per_100k)) +
+  geom_line(aes(color = country_name))
